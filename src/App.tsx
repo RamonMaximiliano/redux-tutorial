c044c31a2d1c590ceb1a94e9c1ce73998c2d36e3
RamonMaximiliano <ramon_max1986@hotmail.com>
2025-03-27 17:33:07 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -23,22 +23,12 @@ export default App
 
 /*
 
-Criar uma Login page
+Based on:
 
-Que após logado mostra o nome do user logado na success Message
-
-E altera os items no nome dele, ex favorite Heroes, na Heroes message
-
-Criar um painel de card com os Heroes para ele selecionar 
-
-
-42 min
 https://www.youtube.com/watch?v=u99tNt3TZf8&t=38s&ab_channel=Rocketseat
 
-17 min
-https://www.youtube.com/watch?v=HGMd3dbB-lM&ab_channel=Coffstack
-
-16 min
 https://www.youtube.com/watch?v=mXI2-gjIZ40&ab_channel=FelipeRocha%E2%80%A2FullStackClub
+
+
 
 */
